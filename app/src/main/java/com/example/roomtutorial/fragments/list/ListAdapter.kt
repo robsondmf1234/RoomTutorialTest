3c15fb51533c0e16c96b35e0c0f5c3e9fe7a82ec
robsondmf1234@gmail.com <Itupeva123@>
2022-10-12 17:59:55 -0300

    Update

--- app/src/main/java/com/example/roomtutorial/fragments/list/ListAdapter.kt
+++ app/src/main/java/com/example/roomtutorial/fragments/list/ListAdapter.kt
@@ -3,6 +3,7 @@ package com.example.roomtutorial.fragments.list
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.roomtutorial.R
